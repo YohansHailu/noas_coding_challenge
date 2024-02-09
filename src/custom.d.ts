d2b9c6ff93889c7902b4cddbc7d0e3b75be89d54
yohans hailu <emailyyohans@gmail.com>
2024-02-09 10:54:25 +0300

    depolyment fix

---
+++ src/custom.d.ts
@@ -0,0 +1,9 @@
+declare module 'react-loading' {
+  const ReactLoading: React.FC<any>;
+  export default ReactLoading;
+}
+
+declare module 'react-loading' {
+  const ImageGallery: React.FC<any>;
+  export default ImageGallery;
+}
--- src/mytypes/react-loading/index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export class ReactLoading extends React.Component<any, any> {
-
-}
-
-export default ReactLoading;
