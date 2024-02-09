78aac3df9427315520d785166e5e4ffdac873c15
yohans hailu <emailyyohans@gmail.com>
2024-02-09 10:42:26 +0300

    small fix; moved api keys to .env

---
+++ src/mytypes/react-loading/index.d.ts
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class ImageGallery extends React.Component<any, any> {
+
+}
+
+export default ImageGallery;
--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -2,16 +2,18 @@ import axios from 'axios'
 
 async function fetchImages(query: string): Promise<any[]> {
 
-  const API_KEY = 'AIzaSyCtcv8617WkLUMq3ng0Krhd10_PhsxNgYY';
+  const IMAGE_SEARH_API_KEY = process.env.IMAGE_SEARH_API_URL || "";
+  const IMAGE_SEARH_API_URL = process.env.IMAGE_SEARH_API_KEY || "";
+
   const SEARCH_ENGINE_ID = '10aa8b2467a8d4864';
-  const QUERY = query;
   const NUM_RESULTS = 10;
 
   const IMAGE_TYPE = 'photo';
   const IMAGE_SIZE = 'xlarge';
   const IMAGE_ASPECT_RATIO = 'widescreen'
+  const QUERY = query;
 
-  const url = `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${SEARCH_ENGINE_ID}&searchType=image&q=${QUERY}&num=${NUM_RESULTS}&imgType=${IMAGE_TYPE}&imgSize=${IMAGE_SIZE}&imgAspect=${IMAGE_ASPECT_RATIO}`;
+  const url = `${IMAGE_SEARH_API_URL}?key=${IMAGE_SEARH_API_KEY}&cx=${SEARCH_ENGINE_ID}&searchType=image&q=${QUERY}&num=${NUM_RESULTS}&imgType=${IMAGE_TYPE}&imgSize=${IMAGE_SIZE}&imgAspect=${IMAGE_ASPECT_RATIO}`;
 
 
 
@@ -22,13 +24,13 @@ async function fetchImages(query: string): Promise<any[]> {
 
 async function fetchAudio(query: string): Promise<string> {
 
-  let api_url = 'https://www.googleapis.com/youtube/v3/search'
-  let api_key = 'AIzaSyAhRLfHo7TzwPJrXDc2Tz2aM_j9ucLhqPw'
-  const response = await axios.get(api_url, {
+  let AUDIO_SEARCH_API_URL = process.env.AUDIO_SEARCH_API_URL || "";
+  let AUDIO_SEARCH_API_KEY = process.env.AUDIO_SEARCH_API_KEY || "";
+  const response = await axios.get(AUDIO_SEARCH_API_URL, {
     params: {
       q: query,
       type: 'video',
-      key: api_key,
+      key: AUDIO_SEARCH_API_KEY,
       videoCategoryId: '10', // Category ID for Music
       maxResults: 1,
       videoDuration: 'short', // Short videos only
